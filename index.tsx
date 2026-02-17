c06e26d76f54fd6be58210a89dd5683c178bca4a
Marcelo S Reis <marcelodasilvareis30@gmail.com>
2026-02-17 18:38:39 -0300

    commit

--- index.tsx
+++ index.tsx
@@ -1,14 +1,18 @@
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { AppComponent } from './src/app.component';
 import { provideRouter, withHashLocation } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './src/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 }).catch(err => console.error(err));
 
